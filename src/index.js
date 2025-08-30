4b90b39293e19aec5d478f909b03ebff945f3d34
bingsa-tech <biljobs@outlook.com>
2025-08-30 00:28:10 -0400

    Update index.js on cors netlify site link

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173", // Vite dev (frontend)
       "http://127.0.0.1:5173",
-      "http://localhost:8080", // Nginx prod
+      "https://health-now-frontend.netlify.app", // netlify site
     ],
     credentials: true,
   })
